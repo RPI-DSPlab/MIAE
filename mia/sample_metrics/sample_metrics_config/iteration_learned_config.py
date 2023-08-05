5d2289669a12249b12ee35e84424d943cc62c6ee
Zhiqi Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-08-04 22:26:40 -0400

    update on sample_metric_configs

--- mia/sample_metrics/sample_metrics_config/iteration_learned_config.py
+++ mia/sample_metrics/sample_metrics_config/iteration_learned_config.py
@@ -8,18 +8,19 @@ class IterationLearnedConfig(ExampleHardnessConfig, ABC):
     """
 
     """first we initialize the default configuration"""
-
-    learned_metric = 'iteration_learned'  # either 'iteration_learned' or 'epoch_learned'
-    # training parameters
-    num_epochs = 100  # number of epochs to train the model
-    crit = 'cross_entropy'
-    optimizer = 'sgd'
-    num_iterations = 15000  # number of iterations to train the model
-    seeds = [9203, 9304, 3456, 5210]  # seed values
-    batch_size = 10000  # batch size
-    lr = 0.04  # learning rate
-    # result parameters
-    save_path = 'il_results'  # directory to save results
-    save_result = True  # save results
+    def __init__(self):
+        super().__init__()
+        self.learned_metric = 'iteration_learned'  # either 'iteration_learned' or 'epoch_learned'
+        # training parameters
+        self.num_epochs = 100  # number of epochs to train the model
+        self.crit = 'cross_entropy'
+        self.optimizer = 'sgd'
+        self.num_iterations = 15000  # number of iterations to train the model
+        self.seeds = [9203, 9304, 3456, 5210]  # seed values
+        self.batch_size = 10000  # batch size
+        self.lr = 0.04  # learning rate
+        # result parameters
+        self.save_path = 'il_results'  # directory to save results
+        self.save_result = True  # save results
 
 
--- mia/sample_metrics/sample_metrics_config/prediction_depth_config.py
+++ mia/sample_metrics/sample_metrics_config/prediction_depth_config.py
@@ -9,15 +9,17 @@ class PredictionDepthConfig(ExampleHardnessConfig, ABC):
 
     """first we initialize the default configuration"""
 
-    # training parameters
-    num_epochs = 100  # number of epochs to train the model
-    crit = 'cross_entropy'
-    optimizer = 'sgd'
-    num_iterations = 15000  # number of iterations to train the model
-    seeds = [9203, 9304, 3456, 5210]  # seed values
-    batch_size = 10000  # batch size
-    lr = 0.04  # learning rate
-    # result parameters
-    save_path = 'il_results'  # directory to save results
-    save_result = True  # save results
-    knn_k = 30  # k nearest neighbors of knn classifier
+    def __init__(self):
+        # training parameters
+        super().__init__()
+        self.num_epochs = 100  # number of epochs to train the model
+        self.crit = 'cross_entropy'
+        self.optimizer = 'sgd'
+        self.num_iterations = 15000  # number of iterations to train the model
+        self.seeds = [9203, 9304, 3456, 5210]  # seed values
+        self.batch_size = 10000  # batch size
+        self.lr = 0.04  # learning rate
+        # result parameters
+        self.save_path = 'il_results'  # directory to save results
+        self.save_result = True  # save results
+        self.knn_k = 30  # k nearest neighbors of knn classifier
