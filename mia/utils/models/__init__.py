a2134cd85d1506acdfbb2b0da17df24a34b41823
Zhiqi Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-09-02 11:43:20 -0400

    update dataset and model

--- mia/utils/models/__init__.py
+++ mia/utils/models/__init__.py
@@ -1,3 +1,4 @@
 from mia.utils.models.conv_net import ConvNet
 from mia.utils.models.vgg16 import VGG16
 from mia.utils.models.wide_resnet import WideResNet
+from mia.utils.models.base import BaseModel
