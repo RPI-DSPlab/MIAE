21ef39e6e75b6ca7f2add3cd66706bb7cb28bc9f
ZhiqiEliWang <eliwang2332@sina.com>
2024-06-07 14:52:28 -0400

    Adding support to CINIC10

--- experiment/mia_comp/datasets.py
+++ experiment/mia_comp/datasets.py
@@ -49,20 +49,3 @@ def get_cifar100(aug: bool = True) -> ConcatDataset:
     return ConcatDataset([trainset, testset])
 
 
-def get_cinic10(aug: bool = True) -> ConcatDataset:
-    mean = [0.47889522, 0.47227842, 0.43047404]
-    std = [0.24205776, 0.23828046, 0.25874835]
-    regular_transform = T.Compose([T.Resize(32), T.CenterCrop(32), T.ToTensor(),
-                                   T.Normalize(mean=mean, std=std)
-                                   ])
-
-    augmentation_transform = T.Compose([T.RandomHorizontalFlip(), T.RandomCrop(32, padding=4), T.transforms.ToTensor(),
-                                        T.Normalize(mean=mean, std=std)])
-
-    transform = augmentation_transform if aug else regular_transform
-
-    trainset = ImageFolder(root='./data/CINIC-10/train', transform=transform)
-
-    testset = ImageFolder(root='./data/CINIC-10/test', transform=transform)
-
-    return ConcatDataset([trainset, testset])
