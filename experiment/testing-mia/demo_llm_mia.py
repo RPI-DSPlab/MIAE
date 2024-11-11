136415b366262937c921c2e5fa3e6daca4f39da4
WangWNico <nicowang0404@gmail.com>
2024-11-11 17:47:24 -0500

    Merged the two attacks in the demo.

--- experiment/testing-mia/demo_llm_mia.py
+++ experiment/testing-mia/demo_llm_mia.py
@@ -9,6 +9,7 @@ from miae.utils.set_seed import set_seed
 from miae.utils.dataset_utils import load_mimir_dataset
 from miae.attacks.base import LLM_ModelAccess
 from miae.attacks_on_llm.mink import MinKProbAttack, MinKAuxiliaryInfo
+from miae.attacks_on_llm.loss import LossAttack, LossAttackAuxiliaryInfo
 
 
 current_dir = os.getcwd()
@@ -81,6 +82,9 @@ def main():
     test = split_dateset['test']
 
     # Attack the model
+    
+    print("Running the minK attack")
+    
     # Default configuration for minK attack
     default_config = {
         "experiment_name": "min_k_prob_attack_experiment",
@@ -121,13 +125,57 @@ def main():
 
     # Now call _attack
     min_k_result = mink_attack._attack(document=document, probs=probs, tokens=tokens)
-    print(f"the minK result is {min_k_result}")
+    print(f"The minK result is: {min_k_result} \n")
 
     if min_k_result < threshold:
         print("The document is classified as a member.")
     else:
         print("The document is classified as a non-member.")
 
+    print("Running the loss attack")
+
+    # Attack configuration for LossAttack
+    loss_attack_config = {
+        "experiment_name": "loss_attack_experiment",
+        "base_model": "EleutherAI/pythia-160m",
+        "dataset_member": "the_pile",
+        "dataset_nonmember": "the_pile",
+        "min_words": 100,
+        "max_words": 200,
+        "max_tokens": 512,
+        "max_data": 100000,
+        "output_name": "loss_attack_results",
+        "n_samples": 1000,
+        "blackbox_attacks": ["loss"],
+        "env_config": {
+            "results": "results_loss",
+            "device": "cuda:0",
+            "device_aux": "cuda:0"
+        },
+        "dump_cache": False,
+        "load_from_cache": False,
+        "load_from_hf": True,
+        "batch_size": 50,
+        "threshold": 2.6  # Adjust based on testing
+    }
+    
+    loss_info = LossAttackAuxiliaryInfo(loss_attack_config)
+    config_dict = loss_info.save_config_to_dict()
+    
+    # Initialize LossAttack
+    loss_attack = LossAttack(target_model, config_dict)
+
+    # Select a document from the test dataset
+    document = test[0]['text']
+    inputs = tokenizer(document, return_tensors="pt").to(device)
+
+    # Get token IDs instead of strings
+    tokens = tokenizer.encode(document)
+
+    # Run the LossAttack
+    result = loss_attack._attack(document=document, tokens=tokens)
+    print(f"Threshold: {loss_attack.threshold}")
+    print(f"The loss membership inference result is: {'member' if result == 1 else 'non-member'}")
 
 if __name__ == "__main__":
     main()
